57aac98643e7745cb29da63a0c8a905a3fc4e21e
Omotayo Omoseebi <omoseebiomotayo@gmail.com>
2023-07-30 21:23:10 +0100

    first commit

--- src/Pages/AddChild/Conponent/AddChild.jsx
+++ src/Pages/AddChild/Conponent/AddChild.jsx
@@ -2,11 +2,18 @@ import React, { useState } from "react";
 import "../../AddChild/Style/AddChild.css"; 
 
 export const AddChild = () => {
-  const [childInfo, setChildInfo] = useState({
-    childName: "",
+  const [childData, setChildInfo] = useState({
+    // childName: "",
+    // dateOfBirth: "",
+    // parentName: "",
+    // contactNumber: "",
+    firstName: "",
+    lastName: "",
+    gender: "",
     dateOfBirth: "",
-    parentName: "",
-    contactNumber: "",
+    specialNeedCategory: "",
+    specialNeedDescription: "",
+    homeAddress: "",
   });
 
   const handleChange = (e) => {
@@ -17,9 +24,13 @@ export const AddChild = () => {
     }));
   };
 
+  const currentDate = new Date();
+  currentDate.setFullYear(currentDate.getFullYear() - 3);
+  const maxDate = currentDate.toISOString().split("T")[0];
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Child Info:", childInfo);
+    console.log("Child Info:", childData);
   };
 
   return (
@@ -27,44 +38,76 @@ export const AddChild = () => {
     <div className="container">
       <h1>Add a Child</h1>
       <form className="child-form" onSubmit={handleSubmit}>
-        <label htmlFor="childName">Child's Name:</label>
+        <label htmlFor="firstName">First Name:</label>
         <input
-          type="text"
-          id="childName"
-          name="childName"
-          value={childInfo.childName}
-          onChange={handleChange}
-          required
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={childData.firstName}
+            onChange={handleChange}
         />
+
+        <label htmlFor="lastName">Last Name:</label>
+        <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={childData.lastName}
+            onChange={handleChange}
+        />
+
+        <label htmlFor="sex">Gender:</label>
+        <select
+            type="text"
+            id="sex"
+            name="sex"
+            value={childData.gender}
+            onChange={handleChange}
+        >
+          <option value="">Select</option>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+        </select>
 
         <label htmlFor="dateOfBirth">Date of Birth:</label>
         <input
-          type="date"
-          id="dateOfBirth"
-          name="dateOfBirth"
-          value={childInfo.dateOfBirth}
-          onChange={handleChange}
-          required
+            type="date"
+            id="dateOfBirth"
+            name="dateOfBirth"
+            value={childData.dateOfBirth}
+            onChange={handleChange}
+            max={maxDate}
         />
 
-        <label htmlFor="parentName">Parent's Name:</label>
+        <label htmlFor="specialNeedCategory">Special Need Category:</label>
+        <select
+            type="text"
+            id="specialNeedCategory"
+            name="specialNeedCategory"
+            value={childData.specialNeedCategory}
+            onChange={handleChange}
+        >
+          <option value="">Select</option>
+          <option value="YES">YES</option>
+          <option value="NO">NO</option>
+        </select>
+
+        <label htmlFor="specialNeedDescription">Special Need Description:</label>
         <input
-          type="text"
-          id="parentName"
-          name="parentName"
-          value={childInfo.parentName}
-          onChange={handleChange}
-          required
+            type = "text"
+            id="specialNeedDescription"
+            name="specialNeedDescription"
+            value={childData.specialNeedDescription}
+            onChange={handleChange}
         />
 
-        <label htmlFor="contactNumber">Contact Number:</label>
+        <label htmlFor="homeAddress">Home Address:</label>
         <input
-          type="tel"
-          id="contactNumber"
-          name="contactNumber"
-          value={childInfo.contactNumber}
-          onChange={handleChange}
-          required
+            type = "text"
+            id="homeAddress"
+            name="homeAddress"
+            value={childData.homeAddress}
+            onChange={handleChange}
         />
 
         <button type="submit">Add Child</button>
@@ -73,4 +116,8 @@ export const AddChild = () => {
     </div>
   );
 };
+
+
+
+
 
